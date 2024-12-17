eb81c2ee7170568aa35c0077c47626ff24496925
mohammadAhtisham <mohammadahtisham@mohammadAhtishams-MacBook-Air.local>
2024-12-17 23:08:07 +0530

    Partial-Complete

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Feature from "./components/Feature";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <Navbar />
+      <Hero />
+      <About />
+      <Feature/>
+    </main>
+  );
+};
+
+export default App;
